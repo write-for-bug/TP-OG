b39298a2334e05538123ec68dd54cbcd705a362f
write-for-bug <15058114997@163.com>
2025-07-15 19:10:17 +0800

    2025-7-15

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,4 @@
 
-from .ood_dataset import OODDataset  
-from .class_order_sampler import ClassOrderSampler
+from .ood_dataset import OODDataset
 
 from .class_balanced_sampler import ClassBalancedSampler
